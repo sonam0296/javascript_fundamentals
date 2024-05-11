ac322aaa63cd8ba5616f1fc49fe4881e27d0a459
sonam0296 <sonam43jha@gmail.com>
2024-05-11 22:15:37 +0530

    Binary search

--- DSA/searching/LC:34.js
+++ DSA/searching/LC:34.js
@@ -51,3 +51,92 @@ const nums = [5, 7, 7, 8, 8, 10]; const target = 8
 console.log("First & Last Position of target element -> ", searchRange(nums, target))
 
 // O(logN) Time & o(1) space
+
+
+// Find First and Last position element in sorted array
+
+function findFirstOccurenceOfEle(arr, target){
+    let left = 0
+    let right = arr.length - 1
+    let answer = 0
+    while(left <= right){
+        let mid = Math.floor(left + ((right - left) / 2))
+        if (arr[mid] === target) {
+            answer = mid
+            right = mid - 1
+        }
+        else if (arr[mid] < target){
+            left = mid + 1
+        }
+        else{
+            right = mid - 1
+        }
+    }
+    return answer
+}
+
+console.log(`First Occurence of ${3} is -> ${findFirstOccurenceOfEle([1,2,3,3,5], 3)}`)
+
+function findLastOccurenceOfEle(arr, target){
+    let left = 0
+    let right = arr.length - 1
+    let answer = 0
+    while(left <= right){
+        let mid = Math.floor(left + ((right - left) / 2))
+        if (arr[mid] === target) {
+            answer = mid
+            left = mid + 1
+        }
+        else if (arr[mid] < target){
+            left = mid + 1
+        }
+        else{
+            right = mid - 1
+        }
+    }
+    return answer
+}
+
+console.log(`Last Occurence of ${3} is -> ${findLastOccurenceOfEle([1,2,3,3,3,5], 3)}`)
+
+
+// So in the question we need to find the pair of first & last occurence of element - We will combine above function
+
+function pairOfFirstNLastPosition(arr, target){
+    function searchFirstNLastOccurence(isLeftSearching){
+        let left = 0
+        let right = arr.length - 1
+        // We will keep the track of mid element
+        let answer = 0
+        while(left <=  right){
+            // Find Mid ele
+            let mid = Math.floor(left + ((right-left)/2))
+            // If mid element is target then store mid in answer
+            if (arr[mid] === target) {
+                answer = mid
+                // Checking if target is there in left or right side
+                if (isLeftSearching) {
+                    right = mid - 1
+                }
+                else{
+                    left = mid + 1
+                }
+            }
+            else if(arr[mid] < target){
+                left = mid + 1
+            }
+            else{
+                right = mid - 1
+            }
+        }
+        return answer
+    }
+
+    // We will recursively call for left and right side 
+    const left = searchFirstNLastOccurence(true)
+    const right = searchFirstNLastOccurence(false)
+    return [left, right]
+}
+
+const targetEle = 3
+console.log(`First & Last position of ${targetEle} is -> ${pairOfFirstNLastPosition([1,2,3,3,3,5], targetEle)}`)
--- DSA/searching/binarySearch.js
+++ DSA/searching/binarySearch.js
@@ -7,12 +7,12 @@
  * 
  */
 
-function binarySearching(arr, target){
+function binarySearching(arr, target) {
     // Step 0 - Keep two pointers which will point at start and end of list
     let left = 0
     let right = arr.length - 1
     // Step 1 - Loop till left < right
-    while(left <= right){
+    while (left <= right) {
         // Step 2 - Find middle element and index
         let mid = Math.floor((left + right) / 2)
         // Step 3 - Check if target element is same as arr[mid]
@@ -22,7 +22,7 @@ function binarySearching(arr, target){
         else if (arr[mid] < target) {
             left = mid + 1 // Target is in right side
         }
-        else{
+        else {
             right = mid - 1 // Target is in left side
         }
     }
@@ -30,7 +30,39 @@ function binarySearching(arr, target){
 
 }
 
-const arr = [1,2,3,4,5,6,7,8,9]
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const target = 8
 
-console.log(`Index for ${target} element is at ${binarySearching(arr, target)}`)
+// console.log(`Index for ${target} element is at ${binarySearching(arr, target)}`)
+
+// Example - 1
+
+function binarySearch(arr, target) {
+    // Find the start and end point of arr
+    let left = 0
+    let right = arr.length - 1
+    // Loop till left & right are equal
+    while (left <= right) {
+        // While finding mid value there can we a situation when left & right = 2^31 - 1 which will exceed the integer range for mid value
+        let mid = Math.floor(left + ((right - left) / 2))  // This formula so that our integer does not get overflowed
+        // Compare mid and target
+        if (arr[mid] === target) {
+            return mid
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1
+        }
+        else {
+            right = mid - 1
+        }
+    }
+    return -1
+}
+
+const evenArr = [2, 4, 6, 8, 10, 12, 16, 18]
+
+const oddArr = [3, 5, 7, 9, 11, 15, 17, 19]
+
+const evenTarget = 16
+const oddTarget = 5
+console.log(`Index of element ${evenTarget} is -> ${binarySearch(evenArr, evenTarget)}`)
