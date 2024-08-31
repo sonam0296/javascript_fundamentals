190b699c7f2dc3195f64f2a149784feac0296f04
sonam0296 <sonam43jha@gmail.com>
2024-08-31 16:22:53 +0530

    find nth node from end in LL

---
+++ DSA/linkedList/findNthNodeFromEnd.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Nth to Last Node: Find the nth to last node in a singly linked list.
+ */
+
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+// Don't need to create all the function to add, delete
+
+function findNthToLastNode(head, n){
+    // Using two pointers
+    let left = head
+    let right = head
+    for(let i= 0; i<n;i++){
+        if(left == null) return null
+        left = left.next
+    }
+
+    while(left !==null){
+        left = left.next
+        right = right.next
+    }
+
+    return right
+}
+
+let head = new Node(1)
+head.next = new Node(2)
+head.next.next = new Node(3)
+head.next.next.next = new Node(4)
+head.next.next.next.next = new Node(5)
+head.next.next.next.next.next = new Node(6)
+
+console.log(findNthToLastNode(head, 4))
